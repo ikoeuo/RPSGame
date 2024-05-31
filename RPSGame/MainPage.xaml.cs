3c1314e65f1eb42a5f6fb55fbb25c931d9ec7baf
nick <nikole.phipps12@gmail.com>
2024-05-31 19:19:34 -0400

    IU design done

--- RPSGame/MainPage.xaml.cs
+++ RPSGame/MainPage.xaml.cs
@@ -3,15 +3,36 @@
 public partial class MainPage : ContentPage
 {
 	Random ranGen;
-	int rock = 1;
-	int paper = 2;
-	int scissors =3;
+	int systemChoice;
+	int playerChoice;
+	int systemScore = 0;
+	int playerScore = 0;
 	public MainPage()
 	{
 		InitializeComponent();
         //generate computer choice
-        Random ranGen;
-        Random systemChoice = new Random.Next(1, 4);
+        Random ranGen = new Random();
+        systemChoice = ranGen.Next(1, 4);
+
 	}
+
+	void ImageButton_Click(System.Object sender, System.EventArgs e)
+	{
+
+		NewGameButton.IsEnabled = true;
+
+	if (systemChoice == 1)  //rock is 1
+		{
+			systemPick.Source = ImageSource.FromFile("rock.png");
+		}
+    else if(systemChoice == 2) //paper is 2
+		{
+            systemPick.Source = ImageSource.FromFile("paper.png");
+        }
+    else if(systemChoice == 3) //scissors is 3
+		{
+            systemPick.Source = ImageSource.FromFile("scissors.png");
+        }
+    }
 }
 
