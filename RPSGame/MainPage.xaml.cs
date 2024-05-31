4ac75e52b71725366152fdb5c146cee40c14db8a
nick <nikole.phipps12@gmail.com>
2024-05-31 15:48:29 -0400

    Images added

--- RPSGame/MainPage.xaml.cs
+++ RPSGame/MainPage.xaml.cs
@@ -2,9 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
+	Random ranGen;
+	int rock = 1;
+	int paper = 2;
+	int scissors =3;
 	public MainPage()
 	{
 		InitializeComponent();
+        //generate computer choice
+        Random ranGen;
+        Random systemChoice = new Random.Next(1, 4);
 	}
 }
 
